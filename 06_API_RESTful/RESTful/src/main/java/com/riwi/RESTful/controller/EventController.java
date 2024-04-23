a1e32bec5632a3fdf306815d4c90cd7638411281
Jonathan <suarezpadillajonathan@gmail.com>
2024-04-23 11:27:39 -0500

    acyu

--- 06_API_RESTful/RESTful/src/main/java/com/riwi/RESTful/controller/EventController.java
+++ 06_API_RESTful/RESTful/src/main/java/com/riwi/RESTful/controller/EventController.java
@@ -1,16 +1,11 @@
 package com.riwi.RESTful.controller;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.riwi.RESTful.entity.Events;
 import com.riwi.RESTful.services.service_abstract.IEventServices;
@@ -30,15 +25,16 @@ public class EventController {
         return ResponseEntity.ok(this.eventServices.listAll());
     }
 
-    // @PostMapping()
-    // // public ResponseEntity<Events> create(@RequestBody Events events){
-    // //     if (events.getCapacity() < 0) {
-    // //         System.out.println("Invalid capacity");
-    // //     }if (events.getDate() < LocalDate.now()) {
-            
-    // //     }
-    //     return ResponseEntity.ok(this.eventServices.create(events));
-    // }
+    @PostMapping()
+    public ResponseEntity<Events> create(@RequestBody Events events){
+        if (events.getCapacity() < 0) {
+            System.out.println("Invalid capacity");
+
+        }else {
+            ResponseEntity.ok(this.eventServices.create(events));
+        }
+        return ResponseEntity.noContent().build();
+    }
 
     @GetMapping(path = "/{id}")
     public ResponseEntity<Events> findById(@PathVariable String id){
