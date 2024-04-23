24ab6417249aaccee9847f430ebb2c77f5191d1a
miguelangito <miguelangelespinalospina@gmail.com>
2024-04-23 09:38:14 -0500

    actu

--- 06_API_RESTful/RESTful/src/main/java/com/riwi/RESTful/controller/EventController.java
+++ 06_API_RESTful/RESTful/src/main/java/com/riwi/RESTful/controller/EventController.java
@@ -1,7 +1,5 @@
 package com.riwi.RESTful.controller;
 
-import java.sql.Date;
-import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
